3efa0d3b884bda96bd377fe4ccd7f04b420d6d0c
EC2 Default User <ec2-user@ip-172-31-35-148.ap-northeast-1.compute.internal>
2020-04-17 03:36:09 +0000

    git

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,10 @@
+module ApplicationHelper
+  def full_title(page_name = "")
+    base_title = "HISオリジナルTシャツ"
+    if page_name.empty?
+      base_title
+    else
+      page_name + "|" + base_title
+    end
+  end
+end
